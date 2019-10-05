1b5246cd5a82bc8269a7eefe1f7fe8b64caa6c5a
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-10-05 11:05:24 -0300

    Added some instance to task model

--- todo/lib/shared/models/task_model.dart
+++ todo/lib/shared/models/task_model.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class TaskModel {
   String title;
   String description;
-  DateTime date;
 
-  TaskModel({@required this.title, @required this.description, @required this.date});
+  TaskModel({@required this.title, @required this.description});
 }
