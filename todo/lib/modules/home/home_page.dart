32ff87bdb533897f1a929fec70cefd25f0af2dc0
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-10-05 01:05:44 -0300

    Finished v1 UI application

--- todo/lib/modules/home/home_page.dart
+++ todo/lib/modules/home/home_page.dart
@@ -1,17 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:todo/home/home_bloc.dart';
-import 'package:todo/home/home_provider.dart';
+import 'package:todo/modules/home/home_bloc.dart';
 
 class HomePage extends StatefulWidget {
+  final HomeBloc bloc;
   @override
   _HomePageState createState() => _HomePageState();
+
+  HomePage({@required this.bloc});
+
 }
 
 class _HomePageState extends State<HomePage> {
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    widget.bloc.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    HomeBloc bloc = Provider.of<HomeBloc>(context);
-    return HomeProvider();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => addTask(context),
+        backgroundColor: Colors.black,
+        child: Icon(Icons.add, color: Colors.white),
+      ),
+      body: StreamBuilder<String>(
+        stream: widget.bloc.task,
+        builder: (context, snapshot) {
+          return snapshot.hasData ? Text(snapshot.data) : Text("Empty, sorry =/");
+        },
+      ),
+    );
+  }
+
+  Future<Widget> addTask(context) async {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Adicionar task"),
+            content: TextField(
+              onChanged: widget.bloc.onChangeTask,
+            ),
+            actions: <Widget>[
+              RaisedButton(
+                onPressed: widget.bloc.save,
+              )
+            ],
+          );
+        });
   }
 }
