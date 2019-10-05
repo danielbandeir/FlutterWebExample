db4d931019d270ab4d18e4444047a0ccd82b6e62
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-10-05 11:04:55 -0300

    Added listen in home bloc and save function

--- todo/lib/modules/home/home_bloc.dart
+++ todo/lib/modules/home/home_bloc.dart
@@ -1,21 +1,40 @@
 import 'dart:async';
-import 'dart:html';
 
 import 'package:flutter/material.dart';
 import 'package:todo/shared/models/task_model.dart';
+import 'package:todo/shared/repository/task_repository.dart';
 
 class HomeBloc {
-  StreamController<String> _task = StreamController();
+  final TaskRepository taskRepository;
 
-  Stream<String> get task => _task.stream;
+  String _taskNameValue;
+  String _taskDescriptionValue;
 
-  Function(String) get onChangeTask => _task.sink.add;
+  StreamController<String> _taskName = StreamController<String>.broadcast();
+  StreamController<String> _taskDescription = StreamController<String>();
 
-  void save() async {
-    String value = _task.stream.last.toString();
+  // Stream for stream builder
+  Stream<String> get taskName => _taskName.stream.where((data) => data != null);
+  Stream<String> get taskDescription => _taskDescription.stream.where((data) => data != null);
+
+  // When change the Widget task name, add in the stream
+  Function(String) get onChangeTaskName => _taskName.sink.add;
+  Function(String) get onChangeTaskDescription => _taskDescription.sink.add;
+
+  // Initialize listen when data entrance put in _taskNameValue
+  HomeBloc({@required this.taskRepository}) {
+    taskName.listen((data) => _taskNameValue = data);
+    taskDescription.listen((data) => _taskDescriptionValue = data);
   }
 
+  // Catch values from taskName and description to save in local storage
+  void save() async {
+    print("${_taskNameValue} ${_taskDescriptionValue}");
+  }
+
+  // Dispose stream when out of screen
   void dispose() {
-    _task.close();
+    _taskName.close();
+    _taskDescription.close();
   }
 }
