62189c4bb0625bbb0812e867f4fd875cc75e7c5f
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-10-07 02:13:23 -0300

    Finished dashboard page

--- party_project/lib/modules/dashboard/dashboard_page.dart
+++ party_project/lib/modules/dashboard/dashboard_page.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:party_project/shared/models/person_model.dart';
 
 class DashboardPage extends StatefulWidget {
   @override
@@ -6,6 +9,23 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
+
+  @override
+  void initState() {
+    super.initState();
+}
+
+  Future<List<PersonModel>> createPerson() async {
+    List<PersonModel> person = new List<PersonModel>() ;
+
+    person.add(PersonModel(name: "Paul Gilmore", picture: "assets/images/paul-gilmore.jpg"));
+    person.add(PersonModel(name: "Peter Lloyd", picture: "assets/images/peter-lloyd.jpg"));
+    person.add(PersonModel(name: "Natalya Zaritskyaya", picture: "assets/images/natalya-zaritskaya.jpg"));
+    person.add(PersonModel(name: "Alan Hurt Jr.", picture: "assets/images/alan-hurt-jr.jpg"));
+    person.add(PersonModel(name: "Morgan Sessions", picture: "assets/images/morgan-sessions.jpg"));
+    return person ;
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -34,32 +54,89 @@ class _DashboardPageState extends State<DashboardPage> {
                       topLeft: const Radius.circular(100),
                       topRight: const Radius.circular(100))),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(top: 180),
-                    child: Text(
-                      "Hello,",
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontFamily: 'Roboto',
-                          fontWeight: FontWeight.w300,
-                          fontSize: 22),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 5),
-                    child: Text("Cerys Lowe",
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontFamily: 'Roboto',
-                          fontWeight: FontWeight.w900,
-                          fontSize: 32),),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(top: 180),
+                            child: Text(
+                              "Hello,",
+                              textAlign: TextAlign.start,
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontFamily: 'Roboto',
+                                  fontWeight: FontWeight.w300,
+                                  fontSize: 22),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 5),
+                            child: Text("Cerys Lowe",
+                              textAlign: TextAlign.start,
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontFamily: 'Roboto',
+                                  fontWeight: FontWeight.w900,
+                                  fontSize: 32),),
+                          ),
+                        ],
+                      )
+                    ],
                   ),
                   SizedBox(
                     height: 50,
+                  ),
+                  FutureBuilder(
+                    future: this.createPerson(),
+                    builder: (context, snapshot) {
+                      if(snapshot.hasData) {
+                        return Expanded(
+                          child: ListView.builder(
+                            padding: EdgeInsets.only(left: 120),
+                            scrollDirection: Axis.horizontal,
+                            itemCount: snapshot.data.length,
+                            itemBuilder: (context, index) {
+                              PersonModel person = snapshot.data[index];
+                              return Padding(
+                                padding: EdgeInsets.only(left: 150),
+                                child: Column(
+                                  children: <Widget>[
+                                    Container(
+                                      width: 250,
+                                      height: 350,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(20),
+                                        image: DecorationImage(
+                                            fit: BoxFit.cover,
+                                            image: AssetImage(person.picture)
+                                        ),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.only(top: 20),
+                                      child: Text(person.name,
+                                        textAlign: TextAlign.start,
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontFamily: 'Roboto',
+                                            fontWeight: FontWeight.w900,
+                                            fontSize: 22),),
+                                    )
+                                  ],
+                                ),
+                              );
+                            },
+                          ),
+                        );
+                      } else{
+                        return CircularProgressIndicator();
+                      }
+                    },
                   ),
                 ],
               ),
