d1395d1d09c2e4e7e086b4bc8b0d8505a5a9be34
= <bandeirapbdaniel@gmail.com>
2019-10-07 11:14:14 -0300

    Changed login page

--- party_project/lib/modules/login/login_page.dart
+++ party_project/lib/modules/login/login_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:party_project/modules/dashboard/dashboard_page.dart';
+import 'package:party_project/modules/login/widgets/login_body.dart';
 
 class LoginPage extends StatefulWidget {
   final String tag;
@@ -16,116 +16,7 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Positioned(
-            top: 0,
-            child: Container(
-              width: size.width,
-              height: size.height * .4,
-              child: Hero(
-                tag: widget.tag,
-                child: Image.asset(
-                  "assets/images/party_background.jpg",
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            child: Container(
-              width: size.width,
-              height: size.height * 0.8,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topLeft: const Radius.circular(100),
-                      topRight: const Radius.circular(100))),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(top: 70),
-                    child: Text(
-                      "Hi,",
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontFamily: 'Roboto',
-                          fontWeight: FontWeight.w900,
-                          fontSize: 72),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 20),
-                    child: Text("sign in to continue",
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontFamily: 'Roboto',
-                          fontWeight: FontWeight.w500,
-                          fontSize: 32),),
-                  ),
-                  SizedBox(
-                    height: 100,
-                  ),
-                  Container(
-                    width: 550,
-                    child: TextField(
-                      decoration: InputDecoration(
-                          hintText: "E-mail",
-                          hintStyle: TextStyle(color: Colors.black, fontSize: 32),
-                          enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black)
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black)
-                          )
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    width: 550,
-                    child: TextField(
-                      decoration: InputDecoration(
-                          hintText: "Password",
-                          hintStyle: TextStyle(color: Colors.black, fontSize: 32),
-                          enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black)
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black)
-                          ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  Container(
-                    width: size.width*0.3,
-                    height: size.height*0.1,
-                    child: RaisedButton(
-                      onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => DashboardPage()));
-                      },
-                      color: Colors.black,
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-
-                      child: Text("Enter",
-                          style: TextStyle(color: Colors.white, fontFamily: 'Roboto', fontSize: 32, fontWeight: FontWeight.w700)),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
+      body: LoginBody(size: size, tag: widget.tag),
     );
   }
 }
