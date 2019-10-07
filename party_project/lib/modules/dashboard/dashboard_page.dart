4d3669c3afa377528b9af74fc5f54b8170e5bde0
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-10-07 01:35:23 -0300

    Added cerys lower picture

--- party_project/lib/modules/dashboard/dashboard_page.dart
+++ party_project/lib/modules/dashboard/dashboard_page.dart
@@ -8,6 +8,82 @@ class DashboardPage extends StatefulWidget {
 class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            top: 0,
+            child: Container(
+              width: size.width,
+              height: size.height * .4,
+              child: Image.asset(
+                "assets/images/party_background.jpg",
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            child: Container(
+              width: size.width,
+              height: size.height * 0.8,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: const Radius.circular(100),
+                      topRight: const Radius.circular(100))),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(top: 180),
+                    child: Text(
+                      "Hello,",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w300,
+                          fontSize: 22),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 5),
+                    child: Text("Cerys Lowe",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w900,
+                          fontSize: 32),),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: size.height*.1,
+            left: size.width*.5-125,
+            child: Container(
+              width: 250,
+              height: 250,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage(
+                    'assets/images/cerys-lowe.jpg',
+                  )
+                ),
+                shape: BoxShape.circle
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
