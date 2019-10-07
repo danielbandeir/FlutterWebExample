e27cb1e1bef1c4706d2fb17f7b545d81fc472211
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2019-10-06 23:01:44 -0300

    Finished main

--- party_project/lib/main.dart
+++ party_project/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:party_project/modules/dashboard/dashboard_page.dart';
 import 'package:party_project/modules/home/home_page.dart';
+import 'package:party_project/modules/login/login_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: '.Party',
       home: HomePage(),
     );
