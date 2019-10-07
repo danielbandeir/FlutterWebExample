87788604e7f40be0f7e15ff51418f59f1c2cb829
= <bandeirapbdaniel@gmail.com>
2019-10-07 10:34:34 -0300

    Finished changes in HomeBody

--- party_project/lib/modules/home/widgets/home_body.dart
+++ party_project/lib/modules/home/widgets/home_body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:party_project/modules/home/widgets/home_background.dart';
-import 'package:party_project/modules/login/login_page.dart';
+import 'package:party_project/modules/home/widgets/home_background_opacity.dart';
+import 'package:party_project/modules/home/widgets/home_credentials.dart';
+import 'package:party_project/modules/home/widgets/home_title.dart';
 
 class HomeBody extends StatefulWidget {
   @override
@@ -15,85 +17,9 @@ class _HomeBodyState extends State<HomeBody> {
     return Stack(
         children: <Widget>[
           HomeBackground(tagBackground: this.tagBackground),
-          Opacity(
-            opacity: 0.5,
-            child: Container(
-              width: double.infinity,
-              height: double.infinity,
-              color: Colors.black,
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 100, left: 50),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text("life is more than words, is",
-                    style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 22,
-                        fontWeight: FontWeight.w300,
-                        fontFamily: 'Roboto')),
-                Text(
-                  "PARTY",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 102,
-                      fontWeight: FontWeight.w900,
-                      fontFamily: 'Roboto'),
-                ),
-              ],
-            ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    width: size.width*0.3,
-                    height: size.height*0.1,
-                    child: RaisedButton(
-                      onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage(tag: tagBackground)));
-                      },
-                      color: Colors.white,
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-
-                      child: Text("Sign in",
-                          style: TextStyle(color: Colors.black, fontFamily: 'Roboto', fontSize: 32, fontWeight: FontWeight.w700)),
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    width: size.width*0.3,
-                    height: size.height*0.1,
-                    child: RaisedButton(
-                      onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage(tag: tagBackground)));
-                      },
-                      color: Colors.transparent,
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15), side: BorderSide(color: Colors.white)),
-
-                      child: Text("Sign up",
-                          style: TextStyle(color: Colors.white, fontFamily: 'Roboto', fontSize: 32, fontWeight: FontWeight.w700)),
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 45,
-              )
-            ],
-          ),
+          HomeBackgroundOpacity(),
+          HomeTitle(),
+          HomeCredentials(size: size, tagBackground: this.tagBackground),
         ],
       );
   }
