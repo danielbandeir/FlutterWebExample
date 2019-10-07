c7ac7a644cded55e73d58bee0091a22ccead67c5
= <bandeirapbdaniel@gmail.com>
2019-10-07 11:21:34 -0300

    Finished LoginBottomBackgroundWidget

--- party_project/lib/modules/login/widgets/login_bottom_background.dart
+++ party_project/lib/modules/login/widgets/login_bottom_background.dart
@@ -1,10 +1,25 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:party_project/modules/login/widgets/login_credentials.dart';
 
-class HomeBottomBackground extends StatelessWidget {
+class LoginBottomBackground extends StatelessWidget {
+  final Size size;
+
+  LoginBottomBackground({@required this.size});
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Positioned(
+            bottom: 0,
+            child: Container(
+              width: size.width,
+              height: size.height * 0.8,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: const Radius.circular(100),
+                      topRight: const Radius.circular(100))),
+              child: LoginCredentials(size: size),
+            ),
+          );
   }
 }
